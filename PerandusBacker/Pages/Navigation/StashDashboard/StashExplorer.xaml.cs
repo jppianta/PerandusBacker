10b510907f61984f7a1806d136a69c5a853e5e7d
jppianta <joaopedro.pianta@gmail.com>
2021-11-19 02:30:09 -0300

    Update Icons

--- PerandusBacker/Pages/Navigation/StashDashboard/StashExplorer.xaml.cs
+++ PerandusBacker/Pages/Navigation/StashDashboard/StashExplorer.xaml.cs
@@ -34,8 +34,10 @@ namespace PerandusBacker.Pages.Navigation.StashDashboard
       LoadData();
     }
 
-    private async void LoadData() {
-      if (!StashManager.HasLoaded) {
+    private async void LoadData()
+    {
+      if (!StashManager.HasLoaded)
+      {
         LoadingControl.IsLoading = true;
 
         await StashManager.LoadStashes();
@@ -54,6 +56,15 @@ namespace PerandusBacker.Pages.Navigation.StashDashboard
       }
 
       TabControl.SelectedIndex = 0;
+    }
+
+    private SymbolIconSource GetIcon(Tab tab)
+    {
+      switch (tab.Info.Type)
+      {
+        case "CurrencyStash": return new SymbolIconSource() { Symbol = Symbol.Placeholder };
+        default: return new SymbolIconSource() { Symbol = Symbol.Placeholder };
+      }
     }
 
     private TabViewItem CreateTab(Tab tab)
